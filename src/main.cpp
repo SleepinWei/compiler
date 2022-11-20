2706726ce4a535db5ed4120011a4a29d7ec9057c
SleepinWei <zyw0241@outlook.com>
2022-11-20 20:30:35 +0800

    M: modify Lexer

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,23 @@
 //#include <QCoreApplication>
 #include"Parser.h"
+#include"Lexer.h"
 #include<iostream>
+#include<fstream>
 
 int main(int argc, char *argv[])
 {
 //    QCoreApplication a(argc, argv);
 
 //    return a.exec();
+    // Lexer
+    Lexer lex;
+    ifstream t("./asset/source.txt");
+    string s((std::istreambuf_iterator<char>(t)), std::istreambuf_iterator<char>());
+    lex.Preprocess(s);
+    lex.analyze();
+    lex.print("./asset/lex.txt");
+    std::cout << "Lex Analysis Done" << '\n';
+
     Parser parser;
     //parser.readGrammarYACC("./asset/grammar_yacc.txt");
     parser.readGrammar("./asset/grammar.txt");
